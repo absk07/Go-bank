2211cce268753c4bc05c161784a33532aae2c844
absk <noobmaster26128@gmail.com>
2024-07-22 01:25:09 +0530

    implemented jwt

--- api/user.go
+++ api/user.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 
 	db "github.com/absk07/Go-Bank/db/sqlc"
@@ -16,7 +18,7 @@ type createUserReq struct {
 	Email    string `json:"email" binding:"required,email"`
 }
 
-func (server *Server) createUser(ctx *gin.Context) {
+func (server *Server) register(ctx *gin.Context) {
 	var req createUserReq
 	if err := ctx.ShouldBindJSON(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, helpers.ErrorResponse(err))
@@ -48,6 +50,54 @@ func (server *Server) createUser(ctx *gin.Context) {
 			"email":    user.Email,
 			"password_changed_at": user.PasswordChangedAt,
 			"created_at": user.CreatedAt,
+		},
+	})
+}
+
+type loginUserReq struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=3"`
+}
+
+func (server *Server) login(ctx *gin.Context) {
+	var req loginUserReq
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, helpers.ErrorResponse(err))
+		return
+	}
+	user, err := server.store.GetUser(ctx, req.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, helpers.ErrorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, helpers.ErrorResponse(err))
+		return
+	}
+	IsPasswordValid := utils.IsPasswordValid(req.Password, user.Password)
+	fmt.Print("body-", req.Password, "user-", user.Password)
+	if !IsPasswordValid {
+		ctx.JSON(http.StatusUnauthorized, gin.H{
+			"error": true,
+			"message": "invalid credentials",
+		})
+		return
+	}
+	var token string
+	token, err = utils.GenerateToken(user.Email, user.Username)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, helpers.ErrorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, gin.H{
+		"success": true,
+		"data": gin.H{
+			"username": user.Username,
+			"fullname": user.Fullname,
+			"email":    user.Email,
+			"password_changed_at": user.PasswordChangedAt,
+			"created_at": user.CreatedAt,
+			"token": token,
 		},
 	})
 }
---
+++ utils/jwt.go
@@ -0,0 +1,51 @@
+package utils
+
+import (
+	"errors"
+	"log"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func GenerateToken(email, username string) (string, error) {
+	config, err := LoadConfig()
+	if err != nil {
+		log.Fatal("Problem loading configs...")
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email":  email,
+		"userId": username,
+		"exp":    time.Now().Add(time.Hour * 2).Unix(),
+	})
+	return token.SignedString([]byte(config.Secret))
+}
+
+func VerifyToken(token string) (string, error) {
+	config, err := LoadConfig()
+	if err != nil {
+		log.Fatal("Problem loading configs...")
+	}
+	parsedToken, err := jwt.Parse(token, func(tkn *jwt.Token) (any, error) {
+		_, isValidSigningMethod := tkn.Method.(*jwt.SigningMethodHMAC)
+		if !isValidSigningMethod {
+			return nil, errors.New("unexpected signing method")
+		}
+		return []byte(config.Secret), nil
+	})
+	if err != nil {
+		return "", errors.New("could not parse token")
+	}
+	isTokenValid := parsedToken.Valid
+	if !isTokenValid {
+		return "", errors.New("invalid token")
+	}
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", errors.New("invalid token claims")
+	}
+	// fmt.Println("Claims", claims)
+	// email := claims["email"].(string)
+	username := claims["username"].(string)
+	return username, nil
+}
